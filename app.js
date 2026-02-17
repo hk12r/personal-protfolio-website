cd0f3ba4fd8571f4d5591ee18da4df179265ef81
hk12r <2024.himani.ruplani@ves.ac.in>
2026-02-17 19:38:37 +0530

    initial commit
    Modified addItem function to dynamically add skills from an array and updated removeItem to manage the index of added items.

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ function changeText() {
     document.getElementById("myParagraph").innerText = "The text has been changed!";
 }
 
-// Task 2: Add new list item
-let count = 1;
+// Task 2: Add new list items dynamically
+let skills = ["Data Structures", "SQL", "C Programming"];
+let index = 0;
+
 function addItem() {
-    let li = document.createElement("li");
-    li.innerText = "New Item " + count++;
-    document.getElementById("myList").appendChild(li);
+    if (index < skills.length) {
+        let li = document.createElement("li");
+        li.innerText = skills[index++];
+        document.getElementById("myList").appendChild(li);
+    } else {
+        alert("All dynamic skills have been added!");
+    }
 }
 
 // Task 3: Remove last list item
@@ -16,11 +22,16 @@ function removeItem() {
     let list = document.getElementById("myList");
     if (list.lastChild) {
         list.removeChild(list.lastChild);
-        count--;
+        if (index > 0) index--;
     }
 }
 
 // Task 4: Change background color
 function changeColor(color) {
     document.body.style.backgroundColor = color;
+}
+
+// Reset background color to original (#f5f7fb)
+function resetColor() {
+    document.body.style.backgroundColor = "#f5f7fb";
 }
